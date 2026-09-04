7496c4e326597ff4ab623892917d9127ca5600a1
sotreselha <sotreselha@gmail.com>
2026-09-04 23:43:39 +0000

    Initial commit: tests/Engine.Tests/NetworkRegistryTests.cs

---
+++ tests/Engine.Tests/NetworkRegistryTests.cs
@@ -0,0 +1,24 @@
+using GridBot.Engine.Networks;
+using Xunit;
+
+namespace GridBot.Engine.Tests;
+
+public class NetworkRegistryTests
+{
+    [Fact]
+    public void GetNetwork_ReturnsEthereumDescriptor()
+    {
+        var registry = new NetworkRegistry();
+        var network = registry.GetNetwork("ethereum-mainnet");
+
+        Assert.Equal("ETH", network.NativeAssetSymbol);
+        Assert.Equal("evm", network.ChainType);
+    }
+
+    [Fact]
+    public void GetAllNetworks_ReturnsAtLeastFourNetworks()
+    {
+        var registry = new NetworkRegistry();
+        Assert.True(registry.GetAllNetworks().Count >= 4);
+    }
+}
